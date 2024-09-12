45315a8b4f7a187126c3f43597313e276d837694
Mattia Trovato <mattiat196@protonmail.com>
2024-09-12 10:18:49 +0200

    creato il progetto con springboot

---
+++ dicedungeons/src/main/java/com/dragons/dicedungeons/DicedungeonsApplication.java
@@ -0,0 +1,13 @@
+package com.dragons.dicedungeons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicedungeonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicedungeonsApplication.class, args);
+	}
+
+}
