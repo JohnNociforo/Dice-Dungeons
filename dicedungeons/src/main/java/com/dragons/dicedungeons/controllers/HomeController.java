56de11e9b5c08f878c601c42660d84c17825f713
JohnNociforo <info.lowjohn@gmail.com>
2024-09-12 15:57:40 +0200

    HomeController aggiornato.

--- dicedungeons/src/main/java/com/dragons/dicedungeons/controllers/HomeController.java
+++ dicedungeons/src/main/java/com/dragons/dicedungeons/controllers/HomeController.java
@@ -3,11 +3,33 @@ package com.dragons.dicedungeons.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
-    @GetMapping("sheet")
-    public String sheet() {
+
+    @GetMapping("creapersonaggio")
+    public String creaPersonaggio() {
         System.out.println("Mapping scheda");
-        return "stylesheet.html";
+        return "schedaPg/stylesheet.html";
     }
+
+    @GetMapping("home")
+    public String home() {
+        System.out.println("Mapping Homepage");
+        return "home/index.html";
+    }
+
+    //QUANDO APRE IL SITO CON URL VUOTO FA REDIRECT ALLA HOMEPAGE
+    //SI PUò RINOMINARE E TOGLIERE PARAMENTRO IN INPUT
+    @GetMapping("")
+	public String home(HttpSession session)
+	{
+
+		//if(session.getAttribute("loggato") == null)
+			//return "redirect:formlogin";
+		//return "home/index.html";
+        return home();
+	}
+
 }
