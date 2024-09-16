b3d48bfb36a15877f6d12e5ce8d54ff40d76dc5e
Mattia Trovato <mattiat196@protonmail.com>
2024-09-16 10:32:27 +0200

    modificato nomeUtente in username nelle query, aggiunto il metodo cercaUtente

--- dicedungeons/src/main/java/com/dragons/dicedungeons/dao/DaoUtenti.java
+++ dicedungeons/src/main/java/com/dragons/dicedungeons/dao/DaoUtenti.java
@@ -8,12 +8,24 @@ public class DaoUtenti {
     private Database db;
 
     public boolean create(String nomeUtente, String email, String password) {
-        String query = "insert into utenti (nomeUtente, email, password) values(?, ?, ?)";
+        String query = "insert into utenti (username, email, password) values(?, ?, ?)";
         return db.update(query, nomeUtente, email, password);
     }
 
     public List<Map<String, String>> readProva() {
         return db.rows("select * from utenti");
-
     }
+
+	public Map<String,String> cercaUtente(String username, String password)
+	{
+		//Lancio la query sul db per verificare che username e password forniti dall'utente siano
+		//corretti. Se lo sono il db restituisce una mappa PIENA
+		String query = "select * from utenti where username = ? and password = ?";
+		Map<String,String> u = db.row(query, username, password);
+		//Se la mappa restituita dal DB è vuota significa che sarà null in quanto
+		//l'utente non è presente nella tabella.
+		//In caso contrario ritorna l'intera riga dell'utente al quale corrispondono username e
+		//password passati come parametri.
+		return u == null ? null : u;
+	}
 }
